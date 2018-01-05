cb0b4f93ea59ff4cdfeb153694cbf4198cde1ef6
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-05 15:50:16 +0100

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,50 +1,62 @@
 #include <cassert>
 #include <string>
 #include <iostream>
+#include <vector>
+
+bool is_prime(const int i) noexcept
+{
+  if (i < 2)
+  {
+    return false;
+  }
+  if (i == 2)
+  {
+    return true;
+  }
+  for (int j=2; j!=i-1; ++j)
+  {
+    if (i % j == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+int do_main(const std::vector<std::string>& args)
+{
+  if (args.size() != 2) return 1;
+  try
+  {
+    std::cout << std::boolalpha << is_prime(std::stoi(args[1])) << '\n';
+  }
+  catch (const std::exception&)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+void test()
+{
+  assert(do_main( { "is_prime"} ) == 1);
+  assert(do_main( { "is_prime", "4"} ) == 0);
+  assert(do_main( { "is_prime", "7"} ) == 0);
+  assert(do_main( { "is_prime", "nonsense"} ) == 1);
+  assert(do_main( { "is_prime", "4", "16" } ) == 1);
+  assert(!is_prime(-1));
+  assert(!is_prime(0));
+  assert(!is_prime(1));
+  assert( is_prime(2));
+  assert( is_prime(3));
+  assert(!is_prime(4));
+  assert( is_prime(5));
+  assert(!is_prime(6));
+  assert( is_prime(7));
+}
 
 int main(int argc, char* argv[]) 
 {
-  if (argc != 2) return 1;
-  try
-  {
-    const int value{std::stoi(argv[1])};
-    // -1: unknown
-    //  0: false
-    //  1: true
-    int is_prime = -1;
-
-    //Trivial cases
-    if (value < 2) is_prime = 0;
-    if (is_prime == -1 && value == 2) is_prime = 1;
-
-    //Complex cases
-    for (int denominator=2; denominator!=value-1; ++denominator)
-    {
-      if (is_prime != -1) break;
-      if (value % denominator == 0)
-      {
-        is_prime = 0;
-      }
-    }
-    if (is_prime == -1) is_prime = 1;
-
-    //Display the result
-    assert(is_prime != -1); //Must be known now
-    if (is_prime == 0)
-    {
-      std::cout << "false\n";
-    }
-    else
-    {
-      std::cout << "true\n";
-    }
-  }
-  catch (const std::invalid_argument&)
-  {
-    return 1;
-  }
-  catch (const std::out_of_range&)
-  {
-    return 1;
-  }
+  test();
+  return do_main(std::vector<std::string>(argv, argv + argc));
 }
