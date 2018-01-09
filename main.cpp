380812de8e0bf8a4664a1e8cb3eaf9ed8137d60c
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-09 16:32:24 +0100

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,50 +1,78 @@
 #include <cassert>
 #include <string>
 #include <iostream>
+#include <vector>
 
-int main(int argc, char* argv[]) 
+/// Checks if a certain value is a prime number
+bool is_prime(const int value) noexcept
 {
-  if (argc != 2) return 1;
+  if (value < 2)
+  {
+    return false;
+  }
+  if (value == 2)
+  {
+    return true;
+  }
+  for (int denominator=2; denominator!=value-1; ++denominator)
+  {
+    if (value % denominator == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+/// Implementation of is_prime main function
+int do_main(const std::vector<std::string>& args) noexcept
+{
+  if (args.size() != 2) return 1;
   try
   {
-    const int value{std::stoi(argv[1])};
-    // -1: unknown
-    //  0: false
-    //  1: true
-    int is_prime = -1;
-
-    //Trivial cases
-    if (value < 2) is_prime = 0;
-    if (is_prime == -1 && value == 2) is_prime = 1;
-
-    //Complex cases
-    for (int denominator=2; denominator!=value-1; ++denominator)
-    {
-      if (is_prime != -1) break;
-      if (value % denominator == 0)
-      {
-        is_prime = 0;
-      }
-    }
-    if (is_prime == -1) is_prime = 1;
-
-    //Display the result
-    assert(is_prime != -1); //Must be known now
-    if (is_prime == 0)
-    {
-      std::cout << "false\n";
-    }
-    else
-    {
-      std::cout << "true\n";
-    }
+    std::cout << std::boolalpha << is_prime(std::stoi(args[1])) << '\n';
   }
-  catch (const std::invalid_argument&)
+  catch (const std::exception&)
   {
     return 1;
   }
-  catch (const std::out_of_range&)
-  {
-    return 1;
-  }
+  return 0;
+}
+
+/// Tests the exit output of the do_main function
+void test_do_main() noexcept
+{
+  assert(do_main( { "is_prime"} ) == 1);
+  assert(do_main( { "is_prime", "4"} ) == 0);
+  assert(do_main( { "is_prime", "7"} ) == 0);
+  assert(do_main( { "is_prime", "nonsense"} ) == 1);
+  assert(do_main( { "is_prime", "4", "16" } ) == 1);
+}
+
+/// Tests the output of the is_prime function
+void test_is_prime() noexcept
+{
+  assert(!is_prime(-1));
+  assert(!is_prime(0));
+  assert(!is_prime(1));
+  assert( is_prime(2));
+  assert( is_prime(3));
+  assert(!is_prime(4));
+  assert( is_prime(5));
+  assert(!is_prime(6));
+  assert( is_prime(7));
+}
+
+/// Runs all tests for this program 
+void test() noexcept
+{
+  test_do_main();
+  test_is_prime();
+}
+
+/// is_prime main function, that also tests its implementation
+int main(int argc, char* argv[]) 
+{
+  test();
+  return do_main(std::vector<std::string>(argv, argv + argc));
 }
