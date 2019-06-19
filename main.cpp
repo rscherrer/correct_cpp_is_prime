ee7635037ad3d84129df094bda19bde9207f12fe
rscherrer <raphael.scherrer@evobio.eu>
2019-06-19 11:46:35 +0200

    isPrime as a separate function

--- main.cpp
+++ main.cpp
@@ -12,7 +12,24 @@ void checkNumArguments(const int &argc)
 }
 
 
+/// Function to determine if an integer is prime
+bool isPrime(const int &value) noexcept
+{
 
+    // A prime number must be more than one
+    if (value <= 1) {
+        return false;
+    }
+    else {
+        // Try to find a divisor
+        for (int divisor = 2; divisor != value / 2 + 1; ++divisor) {
+            if (!value % divisor) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 
 int main(int argc, char* argv[])
@@ -26,40 +43,16 @@ int main(int argc, char* argv[])
         // Convert argument into a number
         const int value{std::stoi(argv[1])};
 
-        // -1: unknown
-        //  0: false
-        //  1: true
-
         // Determine if the number is prime
         const bool isNumberPrime = isPrime(value);
-        
-        int is_prime = -1;
 
-        //Trivial cases
-        if (value < 2) is_prime = 0;
-        if (is_prime == -1 && value == 2) is_prime = 1;
+        // Display the result
+        isNumberPrime ? std::cout << "true\n" : std::cout << "false\n";
 
-        //Complex cases
-        for (int denominator=2; denominator!=value-1; ++denominator)
-        {
-            if (is_prime != -1) break;
-            if (value % denominator == 0)
-            {
-                is_prime = 0;
-            }
-        }
-        if (is_prime == -1) is_prime = 1;
-
-        //Display the result
-        assert(is_prime != -1); //Must be known now
-        if (is_prime == 0)
-        {
-            std::cout << "false\n";
-        }
-        else
-        {
-            std::cout << "true\n";
-        }
+    }
+    catch (const std::runtime_error&)
+    {
+        return 1;
     }
     catch (const std::invalid_argument&)
     {
